751735e74dad0d47c30ca1fa5a1ea703e925ce87
Talaxika <81813961+Talaxika@users.noreply.github.com>
2022-03-27 15:37:57 +0300

    Create ex1.c

---
+++ Day10/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void printBits(unsigned n)
+{
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
+}
+
+int main() {
+    uint32_t num;
+    scanf("%d", &num);
+    bin(num);
+}
